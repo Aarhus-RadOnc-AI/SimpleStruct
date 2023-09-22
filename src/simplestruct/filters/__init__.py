efacfbba7ecafe63d4cb2d6aa96198ab5e6e7dcd
Mathis Rasmussen <mathis.rasmussen@rm.dk>
2023-09-22 11:15:51 +0200

    Add Count edge

--- src/simplestruct/filters/__init__.py
+++ src/simplestruct/filters/__init__.py
@@ -1,4 +1,4 @@
-from .edge_generator import generate_edge_of_structure
+from .edge_generator import generate_edge_of_structure, count_edge_of_structure
 from .resample_structures_to_common_origin import resample_structures_to_common_origin
 from .hausdorff_maps import generate_hausdorff_map
 from .isometric_expand_shrink import isometric_expand_3d
