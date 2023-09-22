85ff3ecbe7f1f3fb69ffb3ff9671af080f51b21a
Mathis Ersted Rasmussen <mathiser@gmail.com>
2023-09-22 15:30:30 +0200

    Hd (#7)
    * HD
    
    * HD
    
    * Fix
    
    * Fix
    
    * Add arbitrary Hausdorff Map summary functions
    
    * Multiprocesing HausdorffMap
    
    * refac
    
    ---------
    
    Co-authored-by: Mathis Rasmussen <mathis.rasmussen@rm.dk>

--- src/simplestruct/filters/hausdorff_maps.py
+++ src/simplestruct/filters/hausdorff_maps.py
@@ -1,14 +1,15 @@
 import functools
-
+from multiprocessing.pool import ThreadPool
 from simplestruct.metrics.hd import HD
 import SimpleITK as sitk
 from typing import List
 import numpy as np
 
 class HausdorffMap:
-    def __init__(self, reference_structure: sitk.Image, other_contours: List[sitk.Image]):
+    def __init__(self, reference_structure: sitk.Image, other_contours: List[sitk.Image], processes=8):
         self.reference_structure = reference_structure
         self.other_contours = other_contours
+        self.processes = processes
         self.hausdorff_map = None
 
     def execute(self):
@@ -18,13 +19,21 @@ class HausdorffMap:
         Assumes that the contour integers are 1, so binarize your images beforehand.
         """
 
+        def process(ref, other):
+            hd = HD(reference_image=ref, other_image=other)
+            return hd.get_distance_matrix_ref_to_other()
+
+        tp = ThreadPool(self.processes)
+        maps = tp.starmap(process, [(self.reference_structure, other) for other in self.other_contours])
+        tp.close()
+        tp.join()
+
         self.hausdorff_map = None
-        for other_contour in self.other_contours:
-            hd = HD(reference_image=self.reference_structure, other_image=other_contour)
+        for map in maps:
             if self.hausdorff_map is None:
-                self.hausdorff_map = hd.get_distance_matrix_ref_to_other()
+                self.hausdorff_map = map
             else:
-                self.hausdorff_map = np.insert(self.hausdorff_map, -1, hd.get_distance_matrix_ref_to_other()[:, -1], axis=1)
+                self.hausdorff_map = np.insert(self.hausdorff_map, -1, map[:, -1], axis=1)
 
     def get_hausdorff_map(self):
         if self.hausdorff_map is None:
@@ -38,7 +47,7 @@ class HausdorffMap:
         if not func:
             func = functools.partial(np.mean, axis=1)
         arr = np.empty([self.hausdorff_map.shape[0], 4])
-
         arr[:, 3] = func(self.hausdorff_map[:, 3:])
+        
         return arr
 
